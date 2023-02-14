154775be4702118b081d687775ca8e2cea3af9c7
Aryan Bansal <aryanbansal2002@gmail.com>
2023-02-14 21:58:43 +0530

    starting redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import './App.css'
+import {
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom"
+import Login from "./pages/Login/Login"
+import Profile from "./pages/Profile/Profile"
+// import Register from "./pages/Register/Register"
 
 function App() {
 	return (
-		<div className="App">
-		</div>
+		<Routes>
+			<Route path="/" element={
+				<Login />
+			} />
+			<Route path="/profile/:userName" element={
+				<Profile />
+			} />
+			{/* <Route path="/*" element={
+				<Login />
+			} /> */}
+			<Route path="/*" element={
+				<Navigate to="/" />
+			} />
+		</Routes>
 	)
 }
 
-export default App;
+export default App
